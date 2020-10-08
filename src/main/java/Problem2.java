60ba830e6b7943c72ccb1652e35c48b534724dd8
bladez001 <muresan_timothy@yahoo.com>
2020-10-08 00:32:53 -0700

    initial commit for hw1

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,7 +1,18 @@
 public class Problem2 {
-
+    // Timothy Muresan
     // Do not change signature (function name, parameters)
     public static void bubbleSort(int[] data) {
         // Add your solution
+        // sorts in ascending order
+        int l = data.length;
+        for (int i = 0; i < l-1; i++) {
+            for (int j = 0; j < l - 1 - i; j++) {
+                if (data[j] > data[j + 1]){
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] =temp;
+                }
+            }
+        }
     }
 }
--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -1,14 +1,44 @@
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class Problem2Test {
 
     // Example test
     // Okay to change
+    // add your tests here
+    // Timothy Muresan
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
-        // add your tests here
+        int inputs[][] = {
+                {},
+                {1},
+                {0, 2},
+                {3, 1},
+                {3, 2, 1},
+                {3, 2, 5, 7},
+                {4, 3, 1},
+                {5, 2, 3, 1, 8}
+        };
+        int answer[][] = {
+                {},
+                {1},
+                {0, 2},
+                {1, 3},
+                {1, 2, 3},
+                {2, 3, 5, 7},
+                {1, 3, 4},
+                {1, 2, 3, 5, 8}
+        };
+
+        assertEquals(inputs.length, answer.length);
+
+        for (int i=0; i<inputs.length; i++) {
+
+            Problem2.bubbleSort(inputs[i]);
+            //System.out.println(Arrays.toString(inputs[i]));
+            assertArrayEquals(inputs[i], answer[i]);
+        }
     }
 }
